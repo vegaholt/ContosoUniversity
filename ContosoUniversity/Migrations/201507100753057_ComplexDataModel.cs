6caeeb6023d1191b51efa3e1f7b65438a9d1434d
vegaholt <vegard.holter@gmail.com>
2015-07-10 10:27:38 +0200

    step_5

---
+++ ContosoUniversity/Migrations/201507100753057_ComplexDataModel.cs
@@ -0,0 +1,93 @@
+namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ComplexDataModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        DepartmentID = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 50),
+                        Budget = c.Decimal(nullable: false, storeType: "money"),
+                        StartDate = c.DateTime(nullable: false),
+                        InstructorID = c.Int(),
+                    })
+                .PrimaryKey(t => t.DepartmentID)
+                .ForeignKey("dbo.Instructors", t => t.InstructorID)
+                .Index(t => t.InstructorID);
+            
+            CreateTable(
+                "dbo.Instructors",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        HireDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.OfficeAssignments",
+                c => new
+                    {
+                        InstructorID = c.Int(nullable: false),
+                        Location = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.InstructorID)
+                .ForeignKey("dbo.Instructors", t => t.InstructorID)
+                .Index(t => t.InstructorID);
+            
+            CreateTable(
+                "dbo.CourseInstructor",
+                c => new
+                    {
+                        CourseID = c.Int(nullable: false),
+                        InstructorID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.CourseID, t.InstructorID })
+                .ForeignKey("dbo.Courses", t => t.CourseID, cascadeDelete: true)
+                .ForeignKey("dbo.Instructors", t => t.InstructorID, cascadeDelete: true)
+                .Index(t => t.CourseID)
+                .Index(t => t.InstructorID);
+
+            // Create  a department for course to point to.
+            Sql("INSERT INTO dbo.Departments (Name, Budget, StartDate) VALUES ('Temp', 0.00, GETDATE())");
+            //  default value for FK points to department created above.
+            AddColumn("dbo.Courses", "DepartmentID", c => c.Int(nullable: false, defaultValue: 1));
+            //AddColumn("dbo.Courses", "DepartmentID", c => c.Int(nullable: false));
+            AlterColumn("dbo.Courses", "Title", c => c.String(maxLength: 50));
+            AlterColumn("dbo.Students", "LastName", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Students", "FirstName", c => c.String(nullable: false, maxLength: 50));
+            CreateIndex("dbo.Courses", "DepartmentID");
+            AddForeignKey("dbo.Courses", "DepartmentID", "dbo.Departments", "DepartmentID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CourseInstructor", "InstructorID", "dbo.Instructors");
+            DropForeignKey("dbo.CourseInstructor", "CourseID", "dbo.Courses");
+            DropForeignKey("dbo.Courses", "DepartmentID", "dbo.Departments");
+            DropForeignKey("dbo.Departments", "InstructorID", "dbo.Instructors");
+            DropForeignKey("dbo.OfficeAssignments", "InstructorID", "dbo.Instructors");
+            DropIndex("dbo.CourseInstructor", new[] { "InstructorID" });
+            DropIndex("dbo.CourseInstructor", new[] { "CourseID" });
+            DropIndex("dbo.OfficeAssignments", new[] { "InstructorID" });
+            DropIndex("dbo.Departments", new[] { "InstructorID" });
+            DropIndex("dbo.Courses", new[] { "DepartmentID" });
+            AlterColumn("dbo.Students", "FirstName", c => c.String(maxLength: 50));
+            AlterColumn("dbo.Students", "LastName", c => c.String(maxLength: 50));
+            AlterColumn("dbo.Courses", "Title", c => c.String());
+            DropColumn("dbo.Courses", "DepartmentID");
+            DropTable("dbo.CourseInstructor");
+            DropTable("dbo.OfficeAssignments");
+            DropTable("dbo.Instructors");
+            DropTable("dbo.Departments");
+        }
+    }
+}
